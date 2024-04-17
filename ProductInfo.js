26c456376bac32f0999b0bbb073e848b6410b41d
Revilyn Joy Salasiban <146410789+rebelenn@users.noreply.github.com>
2024-04-17 13:42:48 +0800

    Create ProductInfo.js

---
+++ ProductInfo.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function ProductInfo({ id, name, price, description, onAddToCart }) {
+  return (
+    <div>
+      <h2>{name}</h2>
+      <p><strong>Price: </strong> ₱{price}</p>
+      <p style={{ textAlign: 'justify' }}><strong>Description: </strong>{description}</p>
+      <button 
+        onClick={() => onAddToCart({ id, name, price })}
+        style={{
+          backgroundColor: 'red',
+          color: '#fff',
+          border: 'none',
+          padding: '10px 20px',
+          borderRadius: '5px',
+          cursor: 'pointer',
+          fontFamily: 'Poppins',
+          transition: 'background-color 0.3s ease',
+        }}
+      >
+        ADD TO CART
+      </button>
+    </div>
+  );
+}
+
+export default ProductInfo;
